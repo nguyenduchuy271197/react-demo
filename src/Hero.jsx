ee3db2c37a3132296f040eecf5f2e498548fcbbd
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-12-27 22:47:47 +0700

    Initial commit

---
+++ src/Hero.jsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <div className="hero">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque,
+      nobis sunt nemo amet dolores consequatur dolorum. Ad porro impedit esse at
+      rem nemo eum odio eius rerum excepturi, accusantium ea dolore vitae culpa
+      libero. Similique aspernatur soluta voluptates ab harum, molestias nihil.
+      Non, dignissimos culpa at ex cupiditate eaque assumenda, voluptate fugit
+      suscipit sint quibusdam, inventore incidunt quaerat facere expedita ipsa
+      eum maxime aliquam quia sunt voluptatibus iure porro. Perferendis nobis
+      quas repellat tempore voluptate deserunt architecto suscipit tempora
+      cumque vel, odio similique impedit reprehenderit inventore quam id, autem
+      et nulla beatae magni atque. Rem aliquam vel ea facere!
+
+      <button></button>
+    </div>
+  );
+}
---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <img
+        src="https://starboard-one.vercel.app/starboard.svg"
+        alt=""
+        className="logo"
+      />
+      <button>Get Started</button>
+    </div>
+  );
+}
